721add1089c13a5c5ad14faf6fd861e108c2d6d1
Macintosh HD <>
2020-08-31 19:22:14 +0700

    react started

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+
+import './App.css';
+
+
+
+const App = () =>  (
+  <Router>
+    <Fragment>
+      <Navbar/>
+        <Route exact path='/' component={Landing}/>
+        <section className="container">
+
+          {/* NOTE Private Route component  */}
+          <Switch>
+            <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login}/>
+          </Switch>
+          
+          
+        </section> 
+
+    </Fragment>
+  </Router>
+ 
+
+);
+
+export default App;
