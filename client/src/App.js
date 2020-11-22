bfa99186f9c9205118535983560efd94c7e70f55
Macintosh HD <>
2020-11-22 22:52:01 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
 
 import Profile from './components/profile/Profile';
 
@@ -60,8 +62,9 @@ const App = () =>  {
             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
             <PrivateRoute exact path='/add-experience' component={AddExperience}/>
             <PrivateRoute exact path='/add-education' component={AddEducation}/>
+            <PrivateRoute exact path='/posts' component={Posts}/>
+            <PrivateRoute exact path='/post/:id' component={Post}/>
 
-            <Route exact path='/posts' component={Posts}/>
 
 
           </Switch>          
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,17 +4,21 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: {posts, loading}}) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
-    return loading ? <Spinner/> : <Fragment>
+    return loading ? <Spinner/> : 
+    
+    <Fragment>
         <h1 className="large text-primary">Posts</h1>
         <p className="lead">
             <i className="fas fa-user">Welcome to the community</i>
         </p>
         {/* SECTION Posts Form */}
+        <PostForm/>
         <div className="posts">
             {posts.map(post => (
                 <PostItem key={post._id} post={post} />
